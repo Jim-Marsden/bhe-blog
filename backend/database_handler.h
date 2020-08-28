f95c873be607726b8ade61797265cd2eb4830e2e
TremblingOdin <pigmekevin@gmail.com>
2020-08-28 17:40:36 -0600

    another name change to fit conventions

--- backend/database_handler.h
+++ backend/database_handler.h
@@ -4,12 +4,12 @@
 // The header functions to handle the server connections, validations, initializations, and requests
 // Last updated 8/28/2020
 
-#ifndef WEBSERVER_DATABASEHANDLER_H
-#define WEBSERVER_DATABASEHANDLER_H
+#ifndef WEBSERVER_DATABASE_HANDLER_H
+#define WEBSERVER_DATABASE_HANDLER_H
 
-namespace sfkg::databasehandler {
+namespace sfkg::database_handler {
 
-    
+
 }
 
-#endif //WEBSERVER_DATABASEHANDLER_H
+#endif //WEBSERVER_DATABASE_HANDLER_H
