56e6c091ac2526512bfab3717873418c8ca51fd2
Jim <jamespmarsden@hotmail.com>
2020-08-28 19:05:03 -0700

    Added Catch2 template/example/placeholder for html_generator.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,28 @@
+#define RESTINIO_TEST_ON
+#ifdef RESTINIO_TEST_ON
 #include <restinio/all.hpp>
+#define CATCH_CONFIG_RUNNER // Configure catch to use your main, and not its own.
+#include <catch2/catch.hpp>
+
 
 restinio::request_handling_status_t handler(restinio::request_handle_t req) {
-  if (restinio::http_method_get() == req->header().method() &&
-      req->header().request_target() == "/") {
-    req->create_response()
-        .append_header(restinio::http_field::server, "RESTinio hello world server")
-        .append_header_date_field()
-        .append_header(restinio::http_field::content_type, "text/plain; charset=utf-8")
-        .set_body("Hello world!")
-        .done();
-    return restinio::request_accepted();
-  }
+  if (restinio::http_method_get() == req->header().method())
+    //if (req->header().request_target() == "/" || req->header().request_target() == "/home") {
+      req->create_response()
+          .append_header(restinio::http_field::server, "RESTinio hello world server")
+          .append_header_date_field()
+          .append_header(restinio::http_field::content_type, "text/plain; charset=utf-8")
+          .set_body(req->header().request_target())
+          .done();
+      return restinio::request_accepted();
+
   return restinio::request_rejected();
 }
 
-int main() {
+
+int main(int argc, char ** argv) {
+  auto const  test_result = Catch::Session().run(argc, argv);
+
   restinio::run(
       restinio::on_this_thread()
           .port(8080).address("localhost")
@@ -22,3 +30,14 @@ int main() {
 
   return 0;
 }
+#else
+#include <boost/beast/http.hpp>
+using namespace boost::beast::http;
+
+
+#include <cstdlib>
+int main(int argc, char ** argv) {
+  auto const  test_result = Catch::Session().run(argc, argv);
+  return EXIT_SUCCESS;
+}
+#endif
---
+++ tests/html_genrator_test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by snizzfox on 8/28/20.
+//
+
+#include <catch2/catch.hpp>
+
+#include "../backend/html_generator.hpp"
+TEST_CASE("Test one"){
+  REQUIRE(true);
+}
