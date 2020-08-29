190658831ced8ed548878e73177a833de671ff22
TremblingOdin <pigmekevin@gmail.com>
2020-08-28 19:12:30 -0600

    Created URI structure and functions

--- backend/database_handler.h
+++ backend/database_handler.h
@@ -7,9 +7,19 @@
 #ifndef WEBSERVER_DATABASE_HANDLER_H
 #define WEBSERVER_DATABASE_HANDLER_H
 
+#include <string>
+
 namespace sfkg::database_handler {
+    struct Uri {
+    public:
+        std::wstring Query_String, Path, Protocol, Host, Port;
 
+        static auto Uri::Parse(const std::wstring &uri) -> Uri;
 
+        auto Database_Available(const sfkg::database_handler::Uri &uri) -> bool;
+
+        
+    };
 }
 
 #endif //WEBSERVER_DATABASE_HANDLER_H
